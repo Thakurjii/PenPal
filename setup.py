496b6d49b5d0a52d9f45e054a534ce70be8cdd43
Gaurav8871 <gauravthakur40127@gmail.com>
2018-11-09 20:21:51 +0530

    Initial Data Uploaded.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='PenPal',
+    version='0.1.0',
+    description='Send Gmail using Command Line.',
+    long_description=readme,
+    author='Gaurav Thakur',
+    author_email='gauravthakur40127@gmail.com',
+    url='https://github.com/Thakurjii/PenPal',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
