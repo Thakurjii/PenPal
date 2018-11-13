a352727c4ef4def3effde1163d334fc3889c493f
Gaurav8871 <gauravthakur40127@gmail.com>
2018-11-13 05:34:53 +0530

    DataBase interaction implemented

--- main/create.py
+++ main/create.py
@@ -1,35 +1,118 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 
 import getpass
 from .database import store
+from .database import retrieve
 
+import smtplib
+import os.path as op
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+from email.mime.multipart import *
+from email.mime.text import *
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+from os.path import basename
+
+#To add new Gmail account
 def new_account():
-    mail_id=int(input(' Enter your Gmail id > '))
+    mail_id=input(' Enter your Gmail id > ')
     password=getpass.getpass(' Password > ')
     cnfm_pass=getpass.getpass(' Confirm Password > ')
     if password == cnfm_pass:
+
         save_dfl=input(' Make it default account [Y/n] >').lower()
         if save_dfl == 'y':
             store(mail_id,password)
+            mail_from_default_acc()
         else:
-            print()
-        #Take input
-        # call send_mail()        
+            mail_from_new_acc(mail_id,password)
     else:
         exit_app=input(''' 
-        Password didn't matched.
-        Press Y to retry or n to exit.
-        [Y/n] > ''').lower()
+Password didn't matched.
+Press Y to retry or n to exit.
+[Y/n] > ''').lower()
         if exit_app == 'y':
             new_account()
         else:
-            print(' >>> Thanks For Using <<<')
+            print('\n>>> Thanks For Using <<<')
 
-def fast_mail_with_atth():
-    pass
+#Send mail with Attachments
+def fast_mail_with_atth(mail_id,password):
+    recv_mail=input(' Message[\'To\'] > ')
+    subject=input(' Message[\'Subject\'] > ')
+    message=input('Enter Message > ')
+    atth=input('Paste Attachment\'s Path Here > ')
+    send_mail(mail_id,password,recv_mail,subject,message,atth)
 
-def fast_mail_without_atth():
-    pass
+def mail_from_default_acc():
+    credentials=retrieve()
+    mail_id,password=credentials[0].rstrip(),credentials[1]
 
-def send_mail(a,b,c,d):
-    pass
+    attach_choice=input('''
+    Do you want to send mail with Attachments        
+    Enter [Y/n] > ''').lower()
+    if attach_choice == 'y':
+        fast_mail_with_atth(mail_id,password)
+    else:
+        if attach_choice == 'n':
+            fast_mail_without_atth(mail_id,password)
+        else:
+            print('Invalid Choice :(')
+
+def mail_from_new_acc(mail_id,password):
+    
+    attach_choice=input('''
+    Do you want to send mail with Attachments        
+    Enter [Y/n] > ''').lower()
+    if attach_choice == 'y':
+        fast_mail_with_atth(mail_id,password)
+    else:
+        if attach_choice == 'n':
+            fast_mail_without_atth(mail_id,password)
+        else:
+            print('Invalid Choice :(')
+
+#Send mail without Attachments
+def fast_mail_without_atth(mail_id,password):
+    recv_mail=input(' Message[\'To\'] > ')
+    subject=input(' Message[\'Subject\'] > ')
+    message=input('Enter Message > ')
+    send_mail(mail_id,password,recv_mail,subject,message)
+
+def send_mail(mail_id,password,recv_mail,subject,message,atth=''):
+    msg = MIMEMultipart()
+    msg['From'] = mail_id
+    msg['To'] = recv_mail
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message))
+
+    confirm = input(' Press Y to confirm or n to exit [Y/n] >').lower()
+    if confirm == 'y':
+        try:
+            with open(atth, "rb") as fil:
+                part = MIMEApplication(
+                    fil.read(),
+                    Name=basename(atth)
+                )
+                # After the atth is closed
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(atth)
+                msg.attach(part)
+
+        except Exception as e:
+            pass
+
+        mailserver = smtplib.SMTP('smtp.gmail.com',587)
+        # identify ourselves to smtp gmail client
+        mailserver.ehlo()
+        # secure our email with tls encryption
+        mailserver.starttls()
+        # re-identify ourselves as an encrypted connection
+        mailserver.ehlo()
+        mailserver.login(mail_id, password)
+
+        mailserver.sendmail(mail_id,recv_mail,msg.as_string())
+
+        mailserver.quit()
+    else :
+        print('\n>>> Thanks For Using <<<')
--- main/database.py
+++ main/database.py
@@ -1,4 +1,28 @@
-#! usr/bin/python3
+#! /usr/bin/python3
 
+import os
+
+#USER_CREDENTIALS_FILE
+FILE_PATH=r'C:/Users/'+os.getlogin()+'/AppData/Local/.PenPal'
+FILE_NAME=os.path.join(FILE_PATH,'database.pnpl')
+#DEFAULT_EXCEPTION_MESSAGE
+MESSAGE='Something went wrong.Please try to add account again.'
+
+#To store credentials file
 def store(mail_id,password):
-    pass
+    if not os.path.exists(FILE_PATH):
+        os.makedirs(FILE_PATH)
+    try:
+        with open(FILE_NAME,'w') as fn:
+            fn.write(mail_id+'\n'+password)
+
+    except Exception:
+        print('Something went wrong.Please try to add account again.')
+
+#To retrieve data from credentials file
+def retrieve():
+    try:
+        with open(FILE_NAME,'r') as fn:
+            return fn.readlines()
+    except Exception as e:
+        print(MESSAGE)
